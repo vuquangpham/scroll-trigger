760d8194d4a0aeed5f96be076b37bed6a139b653
vuquangpham <vuquangpham2909@gmail.com>
2023-07-17 11:35:48 +0700

    chore: change timing function

--- dev/index.js
+++ dev/index.js
@@ -7,6 +7,7 @@ import './Smooth.min.js';
 
 // package info
 import packageInfo from '../package.json';
+import {Logger} from "sass";
 
 // update project information
 const dataTitles = document.querySelectorAll('[data-title]');
@@ -46,11 +47,22 @@ ScrollTrigger.create({
 Smooth.create({
     timeFraction: customTransition,
     // timing:'lerp',
-    timing: function easeInCubic(x){
-        return x * x * x;
+    timing: function easeOutBounce(x){
+        const n1 = 7.5625;
+        const d1 = 2.75;
+        if(x < 1 / d1){
+            return n1 * x * x;
+        }else if(x < 2 / d1){
+            return n1 * (x -= 1.5 / d1) * x + 0.75;
+        }else if(x < 2.5 / d1){
+            return n1 * (x -= 2.25 / d1) * x + 0.9375;
+        }else{
+            return n1 * (x -= 2.625 / d1) * x + 0.984375;
+        }
     },
     onUpdate: (self) => {
         // current.progress = self.lerp(current.progress, update.progress, 0.05);
+        // console.log(self.progress);
         wrapper.style.setProperty('--progress', self.progress);
     },
 });
