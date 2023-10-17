f99503089200ddb781e961bec249e7e1c9f9a3fa
vuquangpham <vuquangpham2909@gmail.com>
2023-10-18 01:30:05 +0700

    feat: demo

--- dev/index.js
+++ dev/index.js
@@ -17,3 +17,63 @@ dataTitles.forEach(e => e.innerHTML = packageInfo.projectName);
 dataDescriptions.forEach(e => e.innerHTML = packageInfo.description);
 
 // code
+
+const backgroundTriggerEl = document.querySelector('[data-trigger-background]');
+const imageTriggerEl = document.querySelector('[data-trigger-image]');
+
+// background animation
+ScrollTrigger.create({
+    trigger: backgroundTriggerEl,
+    start: 'top 100%',
+    end: '+=' + innerHeight,
+    onUpdate: (self) => {
+        self.trigger.setAttribute('style', `--progress:${self.progress}`);
+    },
+});
+
+// image animation
+ScrollTrigger.create({
+    trigger: imageTriggerEl,
+    start: 'top bottom',
+    onUpdate: (self) => {
+        self.trigger.setAttribute('style', `--progress:${self.progress}`);
+    }
+});
+
+// parallax
+ScrollTrigger.create({
+    trigger: '[data-trigger-parallax]',
+    start: 'top top',
+    onUpdate: (self) => {
+        self.trigger.setAttribute('style', `--progress:${self.progress}`);
+    }
+});
+
+// draw svg
+const letterEl = document.querySelector('.letter');
+letterEl.setAttribute('style', `--length:${letterEl.getTotalLength()}`);
+
+ScrollTrigger.create({
+    trigger: '[data-trigger-draw-svg]',
+    start: 'top 50%',
+    end: 'bottom bottom',
+    onUpdate: (self) => {
+        self.trigger.setAttribute('style', `--progress:${self.progress}`);
+        self.trigger.querySelector('[data-progress]').innerHTML = (self.progress * 100).toFixed(2);
+    }
+});
+
+// last demo
+const progressElement = document.querySelector('#demo [data-progress]');
+const visiblePixelElement = document.querySelector('#demo [data-pixel]');
+
+ScrollTrigger.create({
+    trigger: '[data-trigger-demo]',
+    start: 'top bottom',
+    end: 'bottom bottom',
+    markers: true,
+    onUpdate: (self) => {
+        progressElement.innerHTML = (self.progress).toFixed(2);
+        visiblePixelElement.innerHTML = (self.trigger.getBoundingClientRect().height * self.progress).toFixed(2);
+    }
+});
